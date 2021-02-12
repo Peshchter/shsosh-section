d0418c211ab3f9cff1ac9dba878ebf2ec933b802
NoriakiKakyoin <gorohak07@gmail.com>
2021-02-12 21:11:31 +0400

    Я сделаль Антон

--- Heirofant Green/scripts.js
+++ Heirofant Green/scripts.js
@@ -2,7 +2,7 @@ const timer = document.getElementById('timer');
 let time = new Date();
 
 timer.innerHTML = `${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`;
-const tick = () => {
+const tick  = () => {
     time = new Date();
     timer.innerHTML = `${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`}
 setInterval( tick, 1000);
